a54b4fa5c3cde274fb871209bc55dace713adc32
Ben <ben.hq.stephens@gmail.com>
2017-08-05 15:53:16 -0700

    Slowed tweet generation slightly and reorganized twittler.js

--- twittler.js
+++ twittler.js
@@ -1,15 +1,20 @@
-$(document).ready(function(){
+var showTweets = function() {
   var $body = $('body');
   $body.html('');
-
   var index = streams.home.length - 1;
   while(index >= 0){
     var tweet = streams.home[index];
     var $tweet = $('<div></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message);
+    tweet.prettyCreatedAt = moment(tweet.created_at).fromNow();
+    $tweet.text('@' + tweet.user + ': ' + tweet.message + ' - ' + tweet.prettyCreatedAt);
     $tweet.appendTo($body);
     index -= 1;
   }
+}
 
+$(document).ready(function(){
+
+  showTweets();
+  setInterval(showTweets, 1000);
 
 });
