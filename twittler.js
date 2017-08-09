23ea860d1b480a49f86691497c4d929dc642a7c4
Ben <ben.hq.stephens@gmail.com>
2017-08-09 10:39:58 -0700

    Implemented username filtering, reworked tweet refresh function *Added functionality to display tweets as they come in *Reworked the displayTweet function to prepend rather than append to the list *Cleaned up code

--- twittler.js
+++ twittler.js
@@ -1,20 +1,68 @@
+var selected = false;
+var selection = '';
+var $body = $('body');
+
 var showTweets = function() {
-  var $body = $('body');
+
   $body.html('');
   var index = streams.home.length - 1;
   while(index >= 0){
-    var tweet = streams.home[index];
+    displayTweet(streams.home[index]);
+    index -= 1;
+  }
+  $('.username').on('click', selectUser);
+}
+
+var displayTweet = function(tweet) {
+  if (tweet.loaded !== true){
     var $tweet = $('<div></div>');
     tweet.prettyCreatedAt = moment(tweet.created_at).fromNow();
-    $tweet.text('@' + tweet.user + ': ' + tweet.message + ' - ' + tweet.prettyCreatedAt);
-    $tweet.appendTo($body);
-    index -= 1;
+
+    var tweetContent = "<span class = 'username'>"+"@" + tweet.user + "</span>"
+                       +": " + tweet.message + " - " + tweet.prettyCreatedAt;
+    $tweet.addClass(tweet.user);
+    $tweet.html(tweetContent);
+    //$tweet.text('@' + tweet.user + ': ' + tweet.message + ' - ' + tweet.prettyCreatedAt);
+    $tweet.prependTo($body);
+    tweet.loaded = true;
+  }
+}
+
+var refreshTweets = function() {
+  var newTweets = streams.home.filter(function(theTweet){
+    return theTweet.loaded !== true;
+  });
+  if (selected) {
+    newTweets = newTweets.filter(function(theTweet){
+      return (("." + theTweet.user) === selection);
+    })
+  }
+  newTweets.forEach(function(newTweet){
+    displayTweet(newTweet);
+  })
+  $('.username').on('click', selectUser);
+}
+
+var selectUser = function() {
+  var $siblings = $(this).parent().siblings();
+  var $classy = "." + $(this).parent().attr('class');
+
+  if (selected === false){
+
+    $body.children().not($classy).hide();
+    $body.find($classy).show();
+    selected = true;
+    selection = $classy;
+  } else {
+    selected = false;
+    selection = '';
+    $body.children().show();
   }
 }
 
 $(document).ready(function(){
-
   showTweets();
-  setInterval(showTweets, 1000);
+
+  setInterval(refreshTweets, 1000);
 
 });
